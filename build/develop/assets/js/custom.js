bd71291b4e993982af6e40bf460126623a3effbd
= <=>
2024-02-13 21:19:57 +0000

    build develop

--- build/develop/assets/js/custom.js
+++ build/develop/assets/js/custom.js
@@ -9,3 +9,11 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
+
+addSimMessageHandler("web", (data) => {
+    console.log('addSimMessageHandler')
+    const targetWindow = window.parent
+    console.log('window ', window)
+    console.log('targetWindow ', targetWindow)
+    targetWindow.postMessage(data, '*')
+})
